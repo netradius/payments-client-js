19c0af38c86b1c3bbcd31850947486b739ef09b7
Kris Jen <kjen@logicl.io>
2022-08-05 14:13:37 -0600

    add status-code

---
+++ src/http-client/status-code.ts
@@ -0,0 +1,11 @@
+export enum StatusCode {
+    OK = 200,
+    CREATED = 201,
+    NO_CONTENT = 204,
+    BAD_REQUEST = 400,
+    FORBIDDEN = 403,
+    NOT_FOUND = 404,
+    CONFLICT = 409,
+    INTERNAL_ERROR = 500,
+    NOT_IMPLEMENTED = 501
+}
